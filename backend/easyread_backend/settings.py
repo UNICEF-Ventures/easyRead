869c5d25cd444e0f8f5f358c9aeaf113efb24673
Momina Babar <mominababar97@gmail.com>
2025-12-18 16:55:31 +0500

    fix

--- backend/easyread_backend/settings.py
+++ backend/easyread_backend/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables from .env file
 load_dotenv(BASE_DIR.parent / ".env", override=True)
+load_dotenv(BASE_DIR / ".env", override=True)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
