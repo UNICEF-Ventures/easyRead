82e3f07fdfdce4f5c5b362c994c0c26cdca3dcf7
Momina Babar <mominababar97@gmail.com>
2025-12-22 13:45:43 +0500

    fix

--- backend/easyread_backend/settings.py
+++ backend/easyread_backend/settings.py
@@ -26,6 +26,8 @@ USE_X_FORWARDED_HOST = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+SESSION_COOKIE_SAMESITE = "None"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
