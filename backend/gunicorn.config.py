43b99a6f5fa584cb791246bfbff041274207524a
Momina Babar <mominababar97@gmail.com>
2025-12-22 16:37:39 +0500

    try

---
+++ backend/gunicorn.config.py
@@ -0,0 +1,9 @@
+bind = "0.0.0.0:8000"
+workers = 4
+pidfile = 'pidfile'
+errorlog = 'errorlog'
+loglevel = 'info'
+accesslog = 'accesslog'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+certfile = '/etc/letsencrypt/live/easyreadadminserver.ooiplayground.com/fullchain.pem'
+keyfile = '/etc/letsencrypt/live/easyreadadminserver.ooiplayground.com/privkey.pem'
